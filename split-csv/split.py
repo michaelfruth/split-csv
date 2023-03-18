74bdb1ac5e6cc75c7c0883504053a67ea5309fa5
Michael Fruth <Michael.Fruth@uni-passau.de>
2023-03-18 20:43:14 +0100

    Added flag to ignore columns by regex

--- split-csv/split.py
+++ split-csv/split.py
@@ -4,6 +4,7 @@ import csv
 from typing import List, Any, Optional
 from operator import itemgetter
 from itertools import groupby
+import re
 
 DELIMITER = ";"
 
@@ -23,6 +24,20 @@ def read_csv(file: str) -> CSVData:
         data = reader[1:]
     return CSVData(header, data)
 
+
+def ignore_data(csv_data: CSVData, ignore_columns: List[str]) -> None:
+    if not ignore_columns:
+        return
+    ignore_regexes: List[re.Pattern] = [re.compile(column_name) for column_name in ignore_columns]
+
+    column_indices_to_ignore: List[int] = sorted(list({idx for regex in ignore_regexes for idx, header in enumerate(csv_data.header) if regex.match(header)}))
+
+    removed = 0
+    for ignore_column_index in column_indices_to_ignore:
+        del csv_data.header[ignore_column_index - removed]
+        for data in csv_data.data:
+            del data[ignore_column_index - removed]
+        removed += 1
 
 def match_groups_to_indices(csv_data: CSVData, groups: List[int]):
     def is_in_group(group: str) -> bool:
@@ -61,10 +76,13 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("--csv", help="The path to the csv file.", required=True)
     parser.add_argument("--groups", help="The groups to group. For each group, a PDF is created.", nargs="+", required=True)
+    parser.add_argument("--ignore-regexes", help="The columns to ignore. Regex supported.", nargs="+")
     parser.add_argument("--output", help="The output directory in which the exported PDF files are stored", required=True)
 
     args = parser.parse_args()
     csv_data: CSVData = read_csv(args.csv)
+    print(csv_data.header)
+    ignore_data(csv_data, args.ignore_regexes)
     indices: List[int] = match_groups_to_indices(csv_data, args.groups)
     splitted_data = split_data_by_groups(csv_data, indices)
     export_data(splitted_data, args.output)
